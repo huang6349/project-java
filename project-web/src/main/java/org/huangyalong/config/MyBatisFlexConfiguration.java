3a8e96a5e8c94892e3496e115dfb7b261fa4ab50
huang6349 <252683858@qq.com>
2025-06-20 21:34:27 +0800

    :hammer: 重大重构

--- project-web/src/main/java/org/huangyalong/config/MyBatisFlexConfiguration.java
+++ project-web/src/main/java/org/huangyalong/config/MyBatisFlexConfiguration.java
@@ -1,8 +1,10 @@
 package org.huangyalong.config;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.myframework.core.mybatisflex.MyBatisFlexConfigurer;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@MapperScan("org.huangyalong.**.mapper")
 public class MyBatisFlexConfiguration extends MyBatisFlexConfigurer {
 }
