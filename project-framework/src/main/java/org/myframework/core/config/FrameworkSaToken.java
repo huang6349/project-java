a313bcd29b7cffde764ed8091bcdce5a79a5cb24
huang6349 <252683858@qq.com>
2025-06-30 17:11:31 +0800

    :hammer: 重大重构

--- project-framework/src/main/java/org/myframework/core/config/FrameworkSaToken.java
+++ project-framework/src/main/java/org/myframework/core/config/FrameworkSaToken.java
@@ -1,16 +1,45 @@
 package org.myframework.core.config;
 
 import cn.dev33.satoken.config.SaTokenConfig;
-import lombok.val;
+import cn.dev33.satoken.context.SaHolder;
+import cn.dev33.satoken.filter.SaFilterAuthStrategy;
+import cn.dev33.satoken.filter.SaServletFilter;
+import cn.dev33.satoken.router.SaHttpMethod;
+import cn.dev33.satoken.router.SaRouter;
 import org.myframework.core.satoken.interceptor.SaTokenInterceptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class FrameworkSaToken implements WebMvcConfigurer {
+
+    private final SaFilterAuthStrategy beforeAuth = obj -> {
+        // saToken跨域配置
+        SaHolder.getResponse()
+                .setHeader("Access-Control-Allow-Origin", "*")
+                .setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH")
+                .setHeader("Access-Control-Max-Age", "3600")
+                .setHeader("Access-Control-Allow-Headers", "*")
+                // 是否启用浏览器默认XSS防护： 0=禁用 | 1=启用 | 1; mode=block 启用, 并在检查到XSS攻击时，停止渲染页面
+                .setHeader("X-XSS-Protection", "1; mode=block");
+        // 如果是预检请求，则立即返回到前端
+        SaRouter.match(SaHttpMethod.OPTIONS)
+                .back();
+    };
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOriginPatterns("*")
+                .allowedHeaders("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH")
+                .allowCredentials(true)
+                .maxAge(3600L);
+    }
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
@@ -19,9 +48,16 @@ public class FrameworkSaToken implements WebMvcConfigurer {
     }
 
     @Bean
+    SaServletFilter saServletFilter() {
+        return new SaServletFilter()
+                .addInclude("/**")
+                .setBeforeAuth(beforeAuth);
+    }
+
+    @Bean
     @Primary
     SaTokenConfig saTokenConfig() {
-        val config = new SaTokenConfig();
+        var config = new SaTokenConfig();
         config.setIsWriteHeader(Boolean.TRUE);
         config.setIsConcurrent(Boolean.TRUE);
         config.setIsShare(Boolean.TRUE);
