f838665e2c52511db9e6e591f0a3e795b1409afd
huang6349 <252683858@qq.com>
2025-07-08 15:37:22 +0800

    :hammer: 重大重构

--- project-web/src/main/java/org/huangyalong/modules/system/web/UserPasswordController.java
+++ project-web/src/main/java/org/huangyalong/modules/system/web/UserPasswordController.java
@@ -1,30 +1,28 @@
 package org.huangyalong.modules.system.web;
 
-import cn.dev33.satoken.annotation.SaCheckLogin;
-import io.swagger.v3.oas.annotations.Operation;
+import cn.dev33.satoken.stp.StpUtil;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.huangyalong.modules.system.domain.User;
 import org.huangyalong.modules.system.request.UserPasswordBO;
 import org.huangyalong.modules.system.service.UserPasswordService;
-import org.myframework.base.validation.Update;
+import org.myframework.base.response.ApiResponse;
 import org.myframework.base.web.reactive.ReactiveController;
-import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.myframework.base.web.reactive.ReactiveUpdateController;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
 
 @RestController
-@RequestMapping("/user")
-@Tag(name = "用户管理")
-public class UserPasswordController extends ReactiveController<UserPasswordService, User> {
+@RequestMapping("/user/password")
+@Tag(name = "用户密码")
+public class UserPasswordController extends ReactiveController<UserPasswordService, User>
+        implements ReactiveUpdateController<User, UserPasswordBO> {
 
-    @SaCheckLogin
-    @PutMapping("/password")
-    @Operation(summary = "修改密码")
-    public Mono<Boolean> update(@RequestBody @Validated(Update.class) UserPasswordBO userPasswordBO) {
-        return getReactorService()
+    @Override
+    public ApiResponse<Mono<Boolean>> handlerUpdate(UserPasswordBO userPasswordBO) {
+        StpUtil.checkLogin();
+        var data = getReactorService()
                 .update(userPasswordBO);
+        return ApiResponse.ok(data);
     }
 }
