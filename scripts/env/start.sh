7c698ebd35db9087be3099fd546c2e32a791c249
huang6349 <252683858@qq.com>
2025-07-11 17:33:04 +0800

    :hammer: 重大重构

--- scripts/env/start.sh
+++ scripts/env/start.sh
@@ -2,7 +2,7 @@
 WORKDIR=$PWD
 cd $WORKDIR/project-minio/ && sh start.sh
 cd $WORKDIR/project-redis/ && sh start.sh
+cd $WORKDIR/project-mysql/ && sh start.sh
 cd $WORKDIR/project-es/ && sh start.sh
-cd $WORKDIR/project-pg/ && sh start.sh
 cd $WORKDIR/project-nginx/ && sh start.sh
 docker ps |grep project
