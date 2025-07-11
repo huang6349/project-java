6d59d5904a3f398df884f8708d41dabc831db9e5
huang6349 <252683858@qq.com>
2025-07-11 09:17:59 +0800

    :hammer: 重大重构

--- project-web/src/main/resources/db/migration/V0.0.2__init.sql
+++ project-web/src/main/resources/db/migration/V0.0.2__init.sql
@@ -18,22 +18,21 @@ CREATE TABLE IF NOT EXISTS "tb_example"
     "update_time"      timestamp  DEFAULT CURRENT_TIMESTAMP NOT NULL,
     "version"          int8                                 NULL,
     "is_deleted"       varchar(2) DEFAULT '0'               NOT NULL,
-    PRIMARY KEY (id)
+    CONSTRAINT "pk_example" PRIMARY KEY (id)
 );
 
-COMMENT ON COLUMN "tb_example"."id"                        IS '主键';
-COMMENT ON COLUMN "tb_example"."name"                      IS '示例名称';
-COMMENT ON COLUMN "tb_example"."code"                      IS '示例代码';
-COMMENT ON COLUMN "tb_example"."configs"                   IS '配置信息';
-COMMENT ON COLUMN "tb_example"."extras"                    IS '额外信息';
-COMMENT ON COLUMN "tb_example"."desc"                      IS '备注';
-COMMENT ON COLUMN "tb_example"."status"                    IS '示例状态(0-启用, 1-禁用)';
-COMMENT ON COLUMN "tb_example"."create_time"               IS '创建时间';
-COMMENT ON COLUMN "tb_example"."update_time"               IS '更新时间';
-COMMENT ON COLUMN "tb_example"."version"                   IS '更新版本';
-COMMENT ON COLUMN "tb_example"."is_deleted"                IS '是否删除(0-未删, 1-已删)';
-
-COMMENT ON TABLE "tb_example"                              IS '示例信息';
+COMMENT ON COLUMN "tb_example"."id"                         IS '主键';
+COMMENT ON COLUMN "tb_example"."name"                       IS '示例名称';
+COMMENT ON COLUMN "tb_example"."code"                       IS '示例代码';
+COMMENT ON COLUMN "tb_example"."configs"                    IS '配置信息';
+COMMENT ON COLUMN "tb_example"."extras"                     IS '额外信息';
+COMMENT ON COLUMN "tb_example"."desc"                       IS '备注';
+COMMENT ON COLUMN "tb_example"."status"                     IS '示例状态(0-启用, 1-禁用)';
+COMMENT ON COLUMN "tb_example"."create_time"                IS '创建时间';
+COMMENT ON COLUMN "tb_example"."update_time"                IS '更新时间';
+COMMENT ON COLUMN "tb_example"."version"                    IS '更新版本';
+COMMENT ON COLUMN "tb_example"."is_deleted"                 IS '是否删除(0-未删, 1-已删)';
+COMMENT ON TABLE  "tb_example"                              IS '示例信息';
 
 -- ----------------------------
 -- Trigger of tb_example
