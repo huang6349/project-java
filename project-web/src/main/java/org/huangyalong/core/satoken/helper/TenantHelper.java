5af2846831ebc550a028d9a8d6445ad5ce83dd65
huang6349 <252683858@qq.com>
2025-08-29 16:44:12 +0800

    :hammer:重大重构

--- project-web/src/main/java/org/huangyalong/core/satoken/helper/TenantHelper.java
+++ project-web/src/main/java/org/huangyalong/core/satoken/helper/TenantHelper.java
@@ -1,6 +1,9 @@
 package org.huangyalong.core.satoken.helper;
 
+import cn.hutool.core.convert.Convert;
+import cn.hutool.core.lang.Opt;
 import cn.hutool.core.util.ObjectUtil;
+import com.mybatisflex.core.query.QueryChain;
 import org.huangyalong.modules.system.domain.User;
 import org.myframework.core.redis.RedisHelper;
 
@@ -10,14 +13,22 @@ import static org.huangyalong.modules.system.domain.table.UserTableDef.USER;
 
 public final class TenantHelper {
 
-    public static void load(Object message) {
-        if (ObjectUtil.isNotEmpty(message)) {
-            var key = format("user:tenant:{}", message);
-            RedisHelper.delete(key);
-            var tenant = User.create()
+    public static Long fetch(Object id) {
+        if (ObjectUtil.isNotEmpty(id)) {
+            return QueryChain.of(User.class)
                     .select(USER.TENANT_ID)
-                    .where(USER.ID.eq(message))
-                    .oneAs(String.class);
+                    .where(USER.ID.eq(id))
+                    .oneAs(Long.class);
+        } else return null;
+    }
+
+    public static void load(Object id) {
+        if (ObjectUtil.isNotEmpty(id)) {
+            var key = format("user:tenant:{}", id);
+            RedisHelper.delete(key);
+            var tenant = Opt.ofNullable(fetch(id))
+                    .map(Convert::toStr)
+                    .get();
             if (ObjectUtil.isNotEmpty(tenant))
                 RedisHelper.set(key, tenant);
             RedisHelper.expire(key, 30, MINUTES);
