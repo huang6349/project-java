c823bf6eb23e21792667b86bbe084107999bc3f1
huang6349 <252683858@qq.com>
2025-09-29 16:56:55 +0800

    :hammer:重大重构

---
+++ project-web/src/main/java/org/huangyalong/modules/system/domain/System.java
@@ -0,0 +1,44 @@
+package org.huangyalong.modules.system.domain;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.mybatisflex.annotation.Column;
+import com.mybatisflex.annotation.Table;
+import com.mybatisflex.core.handler.JacksonTypeHandler;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+import lombok.experimental.Accessors;
+import org.dromara.autotable.annotation.AutoColumn;
+import org.dromara.autotable.annotation.AutoTable;
+import org.myframework.base.domain.SuperEntity;
+
+import java.util.Map;
+
+import static org.dromara.autotable.annotation.mysql.MysqlTypeConstant.TEXT;
+
+@Data(staticConstructor = "create")
+@ToString(callSuper = true)
+@EqualsAndHashCode(callSuper = true)
+@Accessors(chain = true)
+@AutoTable(comment = "系统信息")
+@Table(value = "tb_system")
+@Schema(name = "系统信息")
+public class System extends SuperEntity<System, Long> {
+
+    @Column(typeHandler = JacksonTypeHandler.class)
+    @JsonIgnore
+    @AutoColumn(comment = "配置信息", type = TEXT)
+    @Schema(description = "配置信息")
+    private Map<String, Object> configs;
+
+    @Column(typeHandler = JacksonTypeHandler.class)
+    @JsonIgnore
+    @AutoColumn(comment = "额外信息", type = TEXT)
+    @Schema(description = "额外信息")
+    private Map<String, Object> extras;
+
+    @AutoColumn(comment = "备注", length = 512)
+    @Schema(description = "备注")
+    private String desc;
+}
---
+++ project-web/src/main/java/org/huangyalong/modules/system/domain/SystemRecord.java
@@ -0,0 +1,44 @@
+package org.huangyalong.modules.system.domain;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.mybatisflex.annotation.Column;
+import com.mybatisflex.annotation.Table;
+import com.mybatisflex.core.handler.JacksonTypeHandler;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+import lombok.experimental.Accessors;
+import org.dromara.autotable.annotation.AutoColumn;
+import org.dromara.autotable.annotation.AutoTable;
+import org.myframework.base.domain.SuperEntity;
+
+import java.util.Map;
+
+import static org.dromara.autotable.annotation.mysql.MysqlTypeConstant.TEXT;
+
+@Data(staticConstructor = "create")
+@ToString(callSuper = true)
+@EqualsAndHashCode(callSuper = true)
+@Accessors(chain = true)
+@AutoTable(comment = "系统记录")
+@Table(value = "tb_system_record")
+@Schema(name = "系统记录")
+public class SystemRecord extends SuperEntity<SystemRecord, Long> {
+
+    @Column(typeHandler = JacksonTypeHandler.class)
+    @JsonIgnore
+    @AutoColumn(comment = "配置信息", type = TEXT)
+    @Schema(description = "配置信息")
+    private Map<String, Object> configs;
+
+    @Column(typeHandler = JacksonTypeHandler.class)
+    @JsonIgnore
+    @AutoColumn(comment = "额外信息", type = TEXT)
+    @Schema(description = "额外信息")
+    private Map<String, Object> extras;
+
+    @AutoColumn(comment = "备注", length = 512)
+    @Schema(description = "备注")
+    private String desc;
+}
