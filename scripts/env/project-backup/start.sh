8d598009a707ed8f793b3cd319666c00c874e3e8
huang6349 <252683858@qq.com>
2025-10-17 13:45:16 +0800

    :hammer:重大重构

---
+++ scripts/env/project-backup/start.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+WORKDIR=$PWD
+PROJECT=${1:-project}
+mkdir -p $WORKDIR/data
+chmod -R 777 $WORKDIR/.
+docker-compose -p ${PROJECT}-backup up -d --build
--- scripts/env/start.sh
+++ scripts/env/start.sh
@@ -6,4 +6,5 @@ cd $WORKDIR/project-minio/ && sh start.sh ${PROJECT}
 cd $WORKDIR/project-redis/ && sh start.sh ${PROJECT}
 cd $WORKDIR/project-es/ && sh start.sh ${PROJECT}
 cd $WORKDIR/project-nginx/ && sh start.sh ${PROJECT}
+cd $WORKDIR/project-backup/ && sh start.sh ${PROJECT}
 docker ps |grep -E ${PROJECT}
