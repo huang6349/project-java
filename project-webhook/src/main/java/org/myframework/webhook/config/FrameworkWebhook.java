152eed41c3626922986f24d6bcea6f6f475691d1
huang6349 <252683858@qq.com>
2025-12-07 22:21:37 +0800

    :hammer:重大重构

---
+++ project-webhook/src/main/java/org/myframework/webhook/config/FrameworkWebhook.java
@@ -0,0 +1,7 @@
+package org.myframework.webhook.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class FrameworkWebhook {
+}
---
+++ project-webhook/src/main/java/org/myframework/webhook/properties/WebhookProperties.java
@@ -0,0 +1,17 @@
+package org.myframework.webhook.properties;
+
+import lombok.Data;
+import lombok.ToString;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+import java.io.Serializable;
+
+@Data
+@Configuration
+@ConfigurationProperties("app.webhook")
+@ToString(callSuper = true)
+public class WebhookProperties implements Serializable {
+
+    private boolean enabled = Boolean.TRUE;
+}
