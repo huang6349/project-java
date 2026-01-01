3158f5e8d81693864de8ccf8d7df1c9cf19b7448
huang6349 <252683858@qq.com>
2026-01-01 15:57:59 +0800

    :hammer:重大重构

---
+++ project-web/src/main/java/org/huangyalong/modules/system/properties/TenantProperties.java
@@ -0,0 +1,17 @@
+package org.huangyalong.modules.system.properties;
+
+import lombok.Data;
+import lombok.ToString;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+import java.io.Serializable;
+
+@Data
+@Configuration
+@ConfigurationProperties("app.tenant")
+@ToString(callSuper = true)
+public class TenantProperties implements Serializable {
+
+    private boolean enabled = Boolean.TRUE;
+}
