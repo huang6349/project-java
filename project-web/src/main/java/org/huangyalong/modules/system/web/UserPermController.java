c6a7f9906de33af02e4a518b2bc58f4f38cdbb4c
huang6349 <252683858@qq.com>
2026-01-02 00:59:30 +0800

    :hammer:重大重构

--- project-web/src/main/java/org/huangyalong/modules/system/web/UserPermController.java
+++ project-web/src/main/java/org/huangyalong/modules/system/web/UserPermController.java
@@ -3,37 +3,45 @@ package org.huangyalong.modules.system.web;
 import cn.hutool.core.lang.Opt;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.Getter;
 import org.huangyalong.modules.system.domain.Perm;
+import org.huangyalong.modules.system.properties.TenantProperties;
 import org.huangyalong.modules.system.request.UserPermBO;
 import org.huangyalong.modules.system.request.UserPermQueries;
 import org.huangyalong.modules.system.response.UserPermVO;
 import org.huangyalong.modules.system.service.UserPermService;
 import org.myframework.base.web.SuperSimpleController;
+import org.myframework.core.exception.BusinessException;
 import org.myframework.core.satoken.annotation.PreAuth;
 import org.myframework.core.satoken.annotation.PreCheckPermission;
 import org.myframework.core.satoken.annotation.PreMode;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Mono;
 
+import static cn.hutool.core.lang.Opt.ofNullable;
+import static org.huangyalong.core.constants.TenantConstants.INVALID;
 import static org.huangyalong.core.satoken.helper.PermHelper.fetch;
 
+@Getter
 @PreAuth(replace = "@user")
 @RestController
 @RequestMapping("/user/perm")
 @Tag(name = "用户权限")
 public class UserPermController extends SuperSimpleController<UserPermService, Perm> {
 
+    @Autowired
+    private TenantProperties properties;
+
     @PreCheckPermission(value = {"{}:query", "{}:view"}, mode = PreMode.OR)
     @GetMapping
     @Operation(summary = "根据主键查询（用户权限）")
-    public Mono<UserPermVO> query(@Validated UserPermQueries queries) {
-        var tenantId = Opt.ofNullable(queries)
-                .map(UserPermQueries::getTenantId)
-                .get();
+    public Mono<UserPermVO> query(UserPermQueries queries) {
+        var tenantId = getTenantId(queries);
         var id = Opt.ofNullable(queries)
                 .map(UserPermQueries::getId)
-                .get();
+                .orElseThrow(() -> new BusinessException("主键不能为空"));
         var permVO = new UserPermVO();
         permVO.setTenantId(tenantId);
         permVO.setId(id);
@@ -47,9 +55,7 @@ public class UserPermController extends SuperSimpleController<UserPermService, P
     @GetMapping("/_all")
     @Operation(summary = "根据主键查询（全部权限）")
     public Mono<UserPermVO> all(@Validated UserPermQueries queries) {
-        var tenantId = Opt.ofNullable(queries)
-                .map(UserPermQueries::getTenantId)
-                .get();
+        var tenantId = getTenantId(queries);
         var id = Opt.ofNullable(queries)
                 .map(UserPermQueries::getId)
                 .get();
@@ -66,5 +72,16 @@ public class UserPermController extends SuperSimpleController<UserPermService, P
     public Mono<Boolean> update(@RequestBody @Validated UserPermBO permBO) {
         return getBaseService()
                 .assoc(permBO);
+    }
+
+    Long getTenantId(UserPermQueries queries) {
+        var enabled = ofNullable(getProperties())
+                .map(TenantProperties::isEnabled)
+                .orElse(Boolean.TRUE);
+        if (enabled) {
+            return ofNullable(queries)
+                    .map(UserPermQueries::getTenantId)
+                    .orElseThrow(() -> new BusinessException("租户不能为空"));
+        } else return INVALID;
     }
 }
--- project-web/src/main/java/org/huangyalong/modules/system/web/UserRoleController.java
+++ project-web/src/main/java/org/huangyalong/modules/system/web/UserRoleController.java
@@ -3,36 +3,45 @@ package org.huangyalong.modules.system.web;
 import cn.hutool.core.lang.Opt;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.Getter;
 import org.huangyalong.core.satoken.helper.RoleHelper;
 import org.huangyalong.modules.system.domain.Role;
+import org.huangyalong.modules.system.properties.TenantProperties;
 import org.huangyalong.modules.system.request.UserRoleBO;
 import org.huangyalong.modules.system.request.UserRoleQueries;
 import org.huangyalong.modules.system.response.UserRoleVO;
 import org.huangyalong.modules.system.service.UserRoleService;
 import org.myframework.base.web.SuperSimpleController;
+import org.myframework.core.exception.BusinessException;
 import org.myframework.core.satoken.annotation.PreAuth;
 import org.myframework.core.satoken.annotation.PreCheckPermission;
 import org.myframework.core.satoken.annotation.PreMode;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Mono;
 
+import static cn.hutool.core.lang.Opt.ofNullable;
+import static org.huangyalong.core.constants.TenantConstants.INVALID;
+
+@Getter
 @PreAuth(replace = "@user")
 @RestController
 @RequestMapping("/user/role")
 @Tag(name = "用户角色")
 public class UserRoleController extends SuperSimpleController<UserRoleService, Role> {
 
+    @Autowired
+    private TenantProperties properties;
+
     @PreCheckPermission(value = {"{}:query", "{}:view"}, mode = PreMode.OR)
     @GetMapping
     @Operation(summary = "根据主键查询")
-    public Mono<UserRoleVO> query(@Validated UserRoleQueries queries) {
-        var tenantId = Opt.ofNullable(queries)
-                .map(UserRoleQueries::getTenantId)
-                .get();
+    public Mono<UserRoleVO> query(UserRoleQueries queries) {
+        var tenantId = getTenantId(queries);
         var id = Opt.ofNullable(queries)
                 .map(UserRoleQueries::getId)
-                .get();
+                .orElseThrow(() -> new BusinessException("主键不能为空"));
         var roleVO = new UserRoleVO();
         roleVO.setTenantId(tenantId);
         roleVO.setId(id);
@@ -46,5 +55,16 @@ public class UserRoleController extends SuperSimpleController<UserRoleService, R
     public Mono<Boolean> update(@RequestBody @Validated UserRoleBO roleBO) {
         return getBaseService()
                 .assoc(roleBO);
+    }
+
+    Long getTenantId(UserRoleQueries queries) {
+        var enabled = ofNullable(getProperties())
+                .map(TenantProperties::isEnabled)
+                .orElse(Boolean.TRUE);
+        if (enabled) {
+            return ofNullable(queries)
+                    .map(UserRoleQueries::getTenantId)
+                    .orElseThrow(() -> new BusinessException("租户不能为空"));
+        } else return INVALID;
     }
 }
